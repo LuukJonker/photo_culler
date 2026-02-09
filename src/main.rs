860460cc1692447b00bc7ea56b37283a731c5b5b
Luuk Jonker <jonkerluuk06@gmail.com>
2026-02-09 22:31:30 +0100

    Working display of a single image in a kind of layout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+mod image_loader;
+
+use std::error::Error;
+
+slint::include_modules!();
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ui = AppWindow::new()?;
+
+    let img = image::open("test.png").expect("DJFKD");
+    ui.set_current_image(image_loader::dynamic_image_to_slint_image(img));
+
+    ui.run()?;
+
+    Ok(())
 }
